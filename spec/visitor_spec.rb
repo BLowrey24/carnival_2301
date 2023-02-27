796da89cd4c3ed1da92c472fd27d650db20974e5
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-27 11:12:18 -0500

    add test: to new visitor spec testing initialize

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,19 @@
+require './lib/visitor'
+
+RSpec.describe Visitor do
+
+  let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(visitor1).to be_a(Visitor)
+    end
+
+    it 'has attributes' do
+      expect(visitor1.name).to eq('Bruce')
+      expect(visitor1.height).to eq(54)
+      expect(visitor1.spending_money).to eq(10)
+      expect(visitor1.preferences).to eq([])
+    end
+  end
+end
