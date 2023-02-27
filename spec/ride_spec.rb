953320b76adf46be02e69514c4fd74a16750e8d1
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-27 11:37:21 -0500

    add test: for #board_rider in Ride spec

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,7 +2,10 @@ require './lib/visitor'
 require './lib/ride'
 
 RSpec.describe Ride do
+
   let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
+  let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
+  let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
 
   describe '#initialize' do
     it 'exists' do
@@ -15,6 +18,22 @@ RSpec.describe Ride do
       expect(ride1.admission_fee).to eq(1)
       expect(ride1.excitement).to eq(:gentle)
       expect(ride1.total_revenue).to eq(0)
+    end
+  end
+
+  descirbe '#board_rider' do
+    it 'adds rider to the ride_log' do
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+
+      expect(ride1.rider_log).to eq([visitor1, visitor2])
+      expect(visitor1.spending_money).to eq(8)
+      expect(visitor4.spending_money).to eq(4)
+      expec(ride1.total_revenue).to eq(3)
     end
   end
 end
