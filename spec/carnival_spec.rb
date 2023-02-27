7aa422612a8995603f11eb8c89d84dc351d5f1fc
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-27 13:12:23 -0500

    add test: for #most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe Carnival do
   let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
   let(:ride2) { Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle }) }
   let(:ride3) { Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling }) }
+  let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
+  let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
+
+  before do
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride2.board_rider(visitor1)
+  end
 
   describe '#initialize' do
     it 'exists' do
@@ -28,6 +36,15 @@ RSpec.describe Carnival do
       
       
       expect(carnival1.rides).to eq([ride1, ride2, ride3])
+    end
+  end
+
+  describe '#most_popular_ride' do
+    it 'returns the most popular ride' do
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+
+      expect(carnival.most_popular_ride).to eq(ride1)
     end
   end
 end
