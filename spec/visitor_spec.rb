da8950cb1f58f3c1faa2e643530c42873c5928c8
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-27 11:21:40 -0500

    add test: to Visitor spec for #tall_enough?

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,6 +3,8 @@ require './lib/visitor'
 RSpec.describe Visitor do
 
   let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
+  let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
+  let(:visitor3) { Visitor.new('Penny', 64, '$15') }
 
   describe '#initialize' do
     it 'exists' do
@@ -18,10 +20,19 @@ RSpec.describe Visitor do
   end
   
   describe '#add_preference' do
-  it 'adds a preference to the preferences array' do
-    visitor1.add_preference(:gentle)
-    visitor1.add_preference(:water)
-    expect(visitor1.preferences).to eq([:gentle, :water])
+    it 'adds a preference to the preferences array' do
+      visitor1.add_preference(:gentle)
+      visitor1.add_preference(:water)
+      expect(visitor1.preferences).to eq([:gentle, :water])
+    end
+  end
+
+  describe '#tall_enough?' do
+    it 'checks the height of the visitor' do
+      expect(visitor1.tall_enough?(54)).to eq(true)
+      expect(visitor2.tall_enough?(54)).to eq(false)
+      expect(visitor3.tall_enough?(54)).to eq(true)
+      expect(visitor1.tall_enough?(64)).to eq(false)
     end
   end
 end
