5cb599f857ba34b05c579c1939ba15335c1add1d
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-27 12:47:12 -0500

    add test: for #add_ride in Carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -5,6 +5,9 @@ require './lib/carnival'
 RSpec.describe Carnival do
 
   let(:carnival1) { Carnival.new(5) }
+  let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
+  let(:ride2) { Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle }) }
+  let(:ride3) { Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling }) }
 
   describe '#initialize' do
     it 'exists' do
@@ -14,6 +17,17 @@ RSpec.describe Carnival do
     it 'has attributes' do
       expect(carnival1.duration).to eq(5)
       expect(carnival1.rides).to eq([])
+    end
+  end
+  
+  def '#add_ride' do
+    it 'adds a ride to the rides array' do
+      add_ride(ride1)
+      add_ride(ride2)
+      add_ride(ride3)
+      
+      
+      expect(carnival1.rides).to eq([ride1, ride2, ride3])
     end
   end
 end
